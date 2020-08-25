7b7bc0991ab385b8412dde5e587cf0018b702a7f
codemasternode <marcin.w@coderspeak.com>
2020-08-25 14:34:11 +0200

    Gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,11 @@
 const path = require(`path`)
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const blogPostTemplate = path.resolve(`src/layouts/post.js`)
+    const { createPage } = actions
+    const blogPostTemplate = path.resolve(`src/layouts/post.js`)
 
-  const result = await graphql(
-      `
+    const result = await graphql(
+        `
       query MyQuery {
         allMdx {
           nodes {
@@ -22,26 +22,19 @@ exports.createPages = async ({ graphql, actions }) => {
                 }
               }
             }
-            excerpt(pruneLength: 20)
+            body
           }
         }
       }
       `
-  )
-  result.data.allMdx.nodes.forEach(post => {
-    createPage({
-      path: `articles/${post.frontmatter.slug}`,
-      component: blogPostTemplate,
-      context: {
-        // Add optional context data to be inserted
-        // as props into the page component..
-        //
-        // The context data can also be used as
-        // arguments to the page GraphQL query.
-        //
-        // The page "path" is always available as a GraphQL
-        // argument.
-      },
+    )
+    result.data.allMdx.nodes.forEach(post => {
+        createPage({
+            path: `articles/${post.frontmatter.slug}`,
+            component: blogPostTemplate,
+            context: {
+                post
+            },
+        })
     })
-  })
 }
--- src/layouts/post.js
+++ src/layouts/post.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
-const PostLayout = () => {
+const PostLayout = ({ pageContext: { post } }) => {
     return (
         <div>
+            <img src={post.frontmatter.featuredImage.childImageSharp.fluid.src} />
             <h1>title</h1>
             <p>author</p>
             <span>image</span>
             <p>Lorem lorem</p>
+            <MDXRenderer>{post.body}</MDXRenderer>
         </div>
     )
 }
